fbe5dc1b0a7c2c90f68abc43d62db2dbbc40b9e8
Aaron Burtnyk <burt202@gmail.com>
2014-01-27 23:00:24 +0000

    adds tests for list item

---
+++ public/tests/js/app/views/genres/list-item-spec.js
@@ -0,0 +1,35 @@
+define([
+	'app/views/genres/list-item',
+	'app/views/genres/vent'
+], function (ListItem, Vent) {
+
+	describe('List Item', function() {
+		describe('Basic Instantiation', function() {
+			it('should be able to be instantiated', function() {
+				var listItem = new ListItem();
+
+				expect(listItem).toBeTruthy();
+			});
+		});
+
+		describe('Edit Icon', function() {
+			it('should trigger an event', function() {
+				var listItem = new ListItem();
+				spyOn(Vent, 'trigger');
+				listItem.editIconEvent();
+
+				expect(Vent.trigger).toHaveBeenCalledWith('edit:genre', listItem.model);
+			});
+		});
+
+		describe('Delete Icon', function() {
+			it('should trigger an event', function() {
+				var listItem = new ListItem();
+				spyOn(Vent, 'trigger');
+				listItem.deleteIconEvent();
+
+				expect(Vent.trigger).toHaveBeenCalledWith('delete:genre', listItem.model);
+			});
+		});
+	});
+});
